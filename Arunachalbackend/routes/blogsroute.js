86d3f05c890770e6a58d787b5286a21a67d24de8
chetanv18~ <chetan@gully2global.com>
2025-08-30 10:18:39 +0530

    blogs updated

--- Arunachalbackend/routes/blogsroute.js
+++ Arunachalbackend/routes/blogsroute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protect, restrictTo } from "../utils/auth.js";
-import { createCategory, getCategories, updateCategory, deleteCategory, createBlog, getBlogs, getBlogById, updateBlog, deleteBlog, getLatest, getBlogPostsOnly, getLinkPostsOnly } from "../controller/BlogsController.js";
+import { createCategory, getCategories, updateCategory, deleteCategory, createBlog, getBlogs, getBlogById, updateBlog, deleteBlog, getLatest, getBlogPostsOnly, getLinkPostsOnly } from "../controller/blogsController.js";
 
 const router = express.Router();
 
@@ -14,13 +14,13 @@ router.delete("/deletecategory/:id", protect, restrictTo("admin"), deleteCategor
 router.post("/createblog", protect, restrictTo("admin"), createBlog);
 router.get("/getallblogs", getBlogs);
 router.get("/getlatest", getLatest);
-router.get("/:id", getBlogById);
-router.put("/:id", protect, restrictTo("admin"), updateBlog);
-router.delete("/:id", protect, restrictTo("admin"), deleteBlog);
+router.get("/singleblog/:id", getBlogById);
+router.put("/updateblogs/:id", protect, restrictTo("admin"), updateBlog);
+router.delete("/deleteblog/:id", protect, restrictTo("admin"), deleteBlog);
 
 // Separate read endpoints for tabs
-router.get("/posts", getBlogPostsOnly);
-router.get("/links", getLinkPostsOnly);
+router.get("/contentblogs/posts", getBlogPostsOnly);
+router.get("/linkblogs/links", getLinkPostsOnly);
 
 export default router;
 
