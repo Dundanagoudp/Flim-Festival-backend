cb1503c1d1c121bd4c5ce904604da63373cfa966
chetanv18~ <chetan@gully2global.com>
2025-09-05 15:24:25 +0530

    videos routes

---
+++ Arunachalbackend/routes/videosRoutes.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { protect, restrictTo } from "../utils/auth.js";
+import {
+  addVideoBlog,
+  deleteVideoBlog,
+  getRawVideo,
+  getRawVideoById,
+  getVideoBlog,
+  getVideoBlogById,
+  getYoutubeVideo,
+  updateVideoBlog,
+} from "../controller/videosController.js";
+
+const videoBlogRoute = express.Router();
+
+videoBlogRoute.post(
+  "/addVideoBLog",
+  protect,
+  restrictTo("admin"),
+  addVideoBlog
+);
+videoBlogRoute.get("/getVideoBlog", getVideoBlog);
+videoBlogRoute.get("/getYoutubeVideo", getYoutubeVideo);
+videoBlogRoute.get("/getuploadedVideo", getRawVideo);
+videoBlogRoute.get("/getuploadedVideoById/:videoId", getRawVideoById);
+videoBlogRoute.get("/getVideoById/:videoId", getVideoBlogById);
+videoBlogRoute.put(
+  "/updateVideo/:videoId",
+  protect,
+  restrictTo("admin", "user"),
+  updateVideoBlog
+);
+videoBlogRoute.delete(
+  "/deleteVideo/:videoId",
+  protect,
+  restrictTo("admin"),
+  deleteVideoBlog
+);
+
+export default videoBlogRoute;
+
+
